f1f1a03fe5fe52b562641333f735434ab4866a92
Sigma88 <sigmamodexpansions@outlook.com>
2020-05-11 00:46:22 +0200

    First Aid

---
+++ SigmaProfessionFilterFirstAid/SigmaProfessionFilterFirstAid.lua
@@ -0,0 +1,36 @@
+local _, L = ...;
+
+hooksecurefunc("TradeSkillFrame_OnShow",
+	function()
+
+		if L.Init2 then
+			return;
+		end
+
+		L.Init2 = true;
+
+		SigmaProfessionFilter[L["PROFESSION"]] = {
+			["LeftMenu"] = {
+				["title"] = L["LEFT_TITLE"];
+				["tooltip"] = L["LEFT_TOOLTIP"];
+			};
+			["Left"] = {
+				[01] = { name = L["LEFT_01_NAME"]; filter = L["LEFT_01_FILTER"]; };
+				[02] = { name = L["LEFT_02_NAME"]; filter = L["LEFT_02_FILTER"]; };
+			};
+			["RightMenu"] = {
+				["title"] = L["RIGHT_TITLE"];
+				["tooltip"] = L["RIGHT_TOOLTIP"];
+			};
+			["Right"] = {
+				[01] = { name = L["RIGHT_01_NAME"]; filter = L["RIGHT_01_FILTER"]; };
+				[02] = { name = L["RIGHT_02_NAME"]; filter = L["RIGHT_02_FILTER"]; };
+				[03] = { name = L["RIGHT_03_NAME"]; filter = L["RIGHT_03_FILTER"]; };
+				[04] = { name = L["RIGHT_04_NAME"]; filter = L["RIGHT_04_FILTER"]; };
+				[05] = { name = L["RIGHT_05_NAME"]; filter = L["RIGHT_05_FILTER"]; };
+				[06] = { name = L["RIGHT_06_NAME"]; filter = L["RIGHT_06_FILTER"]; };
+			};
+		};
+
+	end
+);
