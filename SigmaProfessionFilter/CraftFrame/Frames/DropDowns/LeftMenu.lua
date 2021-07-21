668f75af9a4ab6e1674bea17d8d49f02be7ec391
Sigma88 <sigmamodexpansions@outlook.com>
2021-07-22 01:17:29 +0200

    fix left menu filterspell

--- SigmaProfessionFilter/CraftFrame/Frames/DropDowns/LeftMenu.lua
+++ SigmaProfessionFilter/CraftFrame/Frames/DropDowns/LeftMenu.lua
@@ -84,11 +84,10 @@ end
 
 -- Return the group name if the craft matches the filter otherwise return ""
 function SPF1.LeftMenu:FilterSpell(spellID, groupIndex)
-	if SPF1:Custom("LeftMenu")["Filter"] then
-		return SPF1:Custom("LeftMenu")["Filter"](craftIndex, groupIndex);
+	if SPF1:Custom("LeftMenu")["FilterSpell"] then
+		return SPF1:Custom("LeftMenu")["FilterSpell"](spellID, groupIndex);
 	else
 		if SPF1:GetMenu("Left") then
-			--local craftName = SPF1.baseGetCraftInfo(craftIndex);
 			local firstGroup = SPF1:GetGroupSpell("Left", spellID, 0);
 			
 			if groupIndex == 0 then
