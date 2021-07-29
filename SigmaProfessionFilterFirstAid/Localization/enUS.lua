8b01b5a3f5f39cb1771059537819b51dbbf99552
Sigma88 <sigmamodexpansions@outlook.com>
2021-07-30 01:32:18 +0200

    update firstaid to tbc

--- SigmaProfessionFilterFirstAid/Localization/enUS.lua
+++ SigmaProfessionFilterFirstAid/Localization/enUS.lua
@@ -2,6 +2,7 @@ local _, L = ...;
 
 -- Profession
 L["PROFESSION"] = "First Aid";
+
 -- Left Menu
 L["LEFT_TITLE"] = "All Types";
 L["LEFT_TOOLTIP"] = "Sort recipes by the type of item they produce.";
@@ -11,34 +12,23 @@ L["LEFT_02_NAME"] = "Anti-Venom";
 -- Filters
 L["LEFT_01_FILTER"] = "Bandage";
 L["LEFT_02_FILTER"] = "Venom";
+
 -- Right Menu
 L["RIGHT_TITLE"] = "All Reagents";
 L["RIGHT_TOOLTIP"] = "Sort recipes by the reagents they require.";
-
-hooksecurefunc("TradeSkillFrame_OnShow",
-	function()
-		if not L.Init1 then
-			L.Init1 = true;
-			L.GetItemInfo();
-		end
-	end
-);
-
-function L.GetItemInfo()
-	-- Names
-	L["RIGHT_01_NAME"] = GetItemInfo("\124Hitem:14047:");
-	L["RIGHT_02_NAME"] = GetItemInfo("\124Hitem:4338:");
-	L["RIGHT_03_NAME"] = GetItemInfo("\124Hitem:4306:");
-	L["RIGHT_04_NAME"] = GetItemInfo("\124Hitem:2592:");
-	L["RIGHT_05_NAME"] = GetItemInfo("\124Hitem:2589:");
-	L["RIGHT_06_NAME"] = "Venom Sac";
-	-- Filters
-	L["RIGHT_01_FILTER"] = GetItemInfo("\124Hitem:14529:");
-	L["RIGHT_02_FILTER"] = GetItemInfo("\124Hitem:8544:");
-	L["RIGHT_03_FILTER"] = GetItemInfo("\124Hitem:6450:");
-	L["RIGHT_04_FILTER"] = GetItemInfo("\124Hitem:3530:");
-	L["RIGHT_05_FILTER"] = GetItemInfo("\124Hitem:1251:");
-	L["RIGHT_06_FILTER"] = "Anti-Venom";
-end
-	
-L.GetItemInfo();
+-- Names
+L["RIGHT_01_NAME"] = "Netherweave";
+L["RIGHT_02_NAME"] = "Runecloth";
+L["RIGHT_03_NAME"] = "Mageweave";
+L["RIGHT_04_NAME"] = "Silk";
+L["RIGHT_05_NAME"] = "Wool";
+L["RIGHT_06_NAME"] = "Linen";
+L["RIGHT_07_NAME"] = "Venom Sac";
+-- Filters
+L["RIGHT_01_FILTER"] = "Netherweave";
+L["RIGHT_02_FILTER"] = "Runecloth";
+L["RIGHT_03_FILTER"] = "Mageweave";
+L["RIGHT_04_FILTER"] = "Silk";
+L["RIGHT_05_FILTER"] = "Wool";
+L["RIGHT_06_FILTER"] = "Linen";
+L["RIGHT_07_FILTER"] = "Venom";
