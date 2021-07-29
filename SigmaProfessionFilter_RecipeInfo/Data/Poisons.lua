d5287c8ec73532d94f95dcfc3e1587adfa0e0747
Sigma88 <sigmamodexpansions@outlook.com>
2021-07-29 23:55:10 +0200

    add learnedAt and levels

--- SigmaProfessionFilter_RecipeInfo/Data/Poisons.lua
+++ SigmaProfessionFilter_RecipeInfo/Data/Poisons.lua
@@ -2,6 +2,8 @@ local RI = SigmaProfessionFilter_RecipeInfo;
 
 RI.Data["Poisons"] = {
 	[2835] = {
+		["learnedAt"] = 175;
+		["levels"] = { 0, 175, 200, 225 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 5173; ["numRequired"] = 1; };
 			[2] = { ["itemID"] = 3372; ["numRequired"] = 1; };
@@ -9,6 +11,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 2892;
 	};
 	[2837] = {
+		["learnedAt"] = 215;
+		["levels"] = { 0, 215, 240, 265 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 5173; ["numRequired"] = 2; };
 			[2] = { ["itemID"] = 3372; ["numRequired"] = 1; };
@@ -16,6 +20,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 2893;
 	};
 	[3420] = {
+		["learnedAt"] = 125;
+		["levels"] = { 0, 125, 150, 175 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 2930; ["numRequired"] = 1; };
 			[2] = { ["itemID"] = 3371; ["numRequired"] = 1; };
@@ -23,6 +29,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 3775;
 	};
 	[3421] = {
+		["learnedAt"] = 275;
+		["levels"] = { 0, 275, 300, 325 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 8923; ["numRequired"] = 1; };
 			[2] = { ["itemID"] = 8925; ["numRequired"] = 1; };
@@ -30,6 +38,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 3776;
 	};
 	[5763] = {
+		["learnedAt"] = 150;
+		["levels"] = { 0, 150, 175, 200 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 2928; ["numRequired"] = 1; };
 			[2] = { ["itemID"] = 3371; ["numRequired"] = 1; };
@@ -37,6 +47,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 5237;
 	};
 	[8681] = {
+		["learnedAt"] = 125;
+		["levels"] = { 0, 125, 150, 175 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 2928; ["numRequired"] = 1; };
 			[2] = { ["itemID"] = 3371; ["numRequired"] = 1; };
@@ -44,6 +56,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 6947;
 	};
 	[8687] = {
+		["learnedAt"] = 165;
+		["levels"] = { 0, 165, 190, 215 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 2928; ["numRequired"] = 1; };
 			[2] = { ["itemID"] = 3372; ["numRequired"] = 1; };
@@ -51,6 +65,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 6949;
 	};
 	[8691] = {
+		["learnedAt"] = 205;
+		["levels"] = { 0, 205, 230, 255 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 8924; ["numRequired"] = 2; };
 			[2] = { ["itemID"] = 3372; ["numRequired"] = 1; };
@@ -58,6 +74,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 6950;
 	};
 	[8694] = {
+		["learnedAt"] = 215;
+		["levels"] = { 0, 215, 240, 265 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 8923; ["numRequired"] = 1; };
 			[2] = { ["itemID"] = 3372; ["numRequired"] = 1; };
@@ -65,6 +83,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 6951;
 	};
 	[11341] = {
+		["learnedAt"] = 245;
+		["levels"] = { 0, 245, 270, 295 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 8924; ["numRequired"] = 1; };
 			[2] = { ["itemID"] = 8925; ["numRequired"] = 1; };
@@ -72,6 +92,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 8926;
 	};
 	[11342] = {
+		["learnedAt"] = 285;
+		["levels"] = { 0, 285, 310, 335 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 8924; ["numRequired"] = 2; };
 			[2] = { ["itemID"] = 8925; ["numRequired"] = 1; };
@@ -79,6 +101,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 8927;
 	};
 	[11343] = {
+		["learnedAt"] = 325;
+		["levels"] = { 0, 325, 350, 375 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 8924; ["numRequired"] = 2; };
 			[2] = { ["itemID"] = 8925; ["numRequired"] = 1; };
@@ -86,6 +110,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 8928;
 	};
 	[11357] = {
+		["learnedAt"] = 255;
+		["levels"] = { 0, 255, 280, 305 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 5173; ["numRequired"] = 1; };
 			[2] = { ["itemID"] = 8925; ["numRequired"] = 1; };
@@ -93,6 +119,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 8984;
 	};
 	[11358] = {
+		["learnedAt"] = 295;
+		["levels"] = { 0, 295, 320, 345 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 5173; ["numRequired"] = 2; };
 			[2] = { ["itemID"] = 8925; ["numRequired"] = 1; };
@@ -100,6 +128,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 8985;
 	};
 	[11400] = {
+		["learnedAt"] = 285;
+		["levels"] = { 0, 285, 310, 335 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 8923; ["numRequired"] = 1; };
 			[2] = { ["itemID"] = 8925; ["numRequired"] = 1; };
@@ -107,6 +137,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 9186;
 	};
 	[13220] = {
+		["learnedAt"] = 185;
+		["levels"] = { 0, 185, 210, 235 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 2930; ["numRequired"] = 1; };
 			[2] = { ["itemID"] = 3372; ["numRequired"] = 1; };
@@ -114,6 +146,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 10918;
 	};
 	[13228] = {
+		["learnedAt"] = 225;
+		["levels"] = { 0, 225, 250, 275 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 2930; ["numRequired"] = 1; };
 			[2] = { ["itemID"] = 5173; ["numRequired"] = 1; };
@@ -122,6 +156,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 10920;
 	};
 	[13229] = {
+		["learnedAt"] = 265;
+		["levels"] = { 0, 265, 290, 315 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 8923; ["numRequired"] = 1; };
 			[2] = { ["itemID"] = 8925; ["numRequired"] = 1; };
@@ -129,6 +165,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 10921;
 	};
 	[13230] = {
+		["learnedAt"] = 305;
+		["levels"] = { 0, 305, 330, 355 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 8923; ["numRequired"] = 1; };
 			[2] = { ["itemID"] = 5173; ["numRequired"] = 1; };
@@ -137,6 +175,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 10922;
 	};
 	[25347] = {
+		["learnedAt"] = 300;
+		["levels"] = { 0, 300, 325, 350 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 5173; ["numRequired"] = 2; };
 			[2] = { ["itemID"] = 8925; ["numRequired"] = 1; };
@@ -144,6 +184,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 20844;
 	};
 	[26786] = {
+		["learnedAt"] = 340;
+		["levels"] = { 0, 340, 355, 370 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 2931; ["numRequired"] = 1; };
 			[2] = { ["itemID"] = 5173; ["numRequired"] = 1; };
@@ -152,6 +194,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 21835;
 	};
 	[26892] = {
+		["learnedAt"] = 365;
+		["levels"] = { 0, 365, 390, 415 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 2931; ["numRequired"] = 1; };
 			[2] = { ["itemID"] = 8925; ["numRequired"] = 1; };
@@ -159,6 +203,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 21927;
 	};
 	[26969] = {
+		["learnedAt"] = 345;
+		["levels"] = { 0, 345, 365, 385 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 2931; ["numRequired"] = 1; };
 			[2] = { ["itemID"] = 8925; ["numRequired"] = 1; };
@@ -166,6 +212,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 22053;
 	};
 	[27282] = {
+		["learnedAt"] = 385;
+		["levels"] = { 0, 385, 405, 425 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 2931; ["numRequired"] = 1; };
 			[2] = { ["itemID"] = 8925; ["numRequired"] = 1; };
@@ -173,6 +221,8 @@ RI.Data["Poisons"] = {
 		["creates"] = 22054;
 	};
 	[27283] = {
+		["learnedAt"] = 345;
+		["levels"] = { 0, 345, 370, 395 };
 		["reagents"] = {
 			[1] = { ["itemID"] = 8923; ["numRequired"] = 2; };
 			[2] = { ["itemID"] = 8925; ["numRequired"] = 1; };
