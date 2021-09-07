866189ddd9ce2f5f02339a25de3ff96a33dbc54d
Sigma88 <sigmamodexpansions@outlook.com>
2021-09-08 01:16:13 +0200

    msa comp

---
+++ SigmaProfessionFilter/TradeSkillFrame/Frames/MSACompatibility.lua
@@ -0,0 +1,41 @@
+local SPF2 = SigmaProfessionFilter[2];
+
+function SPF2.DropDownMenu_Create(name, parent)
+	if MSA_DropDownMenu_Create then
+		return MSA_DropDownMenu_Create(name, parent);
+	else
+		return CreateFrame("Frame", nil, parent, "UIDropDownMenuTemplate");
+	end
+end
+
+function SPF2.DropDownMenu_SetWidth(...)
+	if MSA_DropDownMenu_SetWidth then
+		MSA_DropDownMenu_SetWidth(...);
+	else
+		UIDropDownMenu_SetWidth(...);
+	end
+end
+
+function SPF2.DropDownMenu_SetSelectedID(...)
+	if MSA_DropDownMenu_SetSelectedID then
+		MSA_DropDownMenu_SetSelectedID(...);
+	else
+		UIDropDownMenu_SetSelectedID(...);
+	end
+end
+
+function SPF2.DropDownMenu_Initialize(...)
+	if MSA_DropDownMenu_Initialize then
+		MSA_DropDownMenu_Initialize(...);
+	else
+		UIDropDownMenu_Initialize(...);
+	end
+end
+
+function SPF2.DropDownMenu_AddButton(...)
+	if MSA_DropDownMenu_AddButton then
+		MSA_DropDownMenu_AddButton(...);
+	else
+		UIDropDownMenu_AddButton(...);
+	end
+end
